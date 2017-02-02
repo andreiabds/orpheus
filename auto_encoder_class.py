f89adce750fb3adcee4a12d142820cb053667a88
andreiabds <andreia.bdsodre@gmail.com>
2017-02-01 19:14:35 -0800

    batch normalization and path

--- auto_encoder_class.py
+++ auto_encoder_class.py
@@ -1,14 +1,23 @@
 from data_format import build_nn_dataset
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.regularizers import l2
+import datetime
 from keras import backend as K
 from keras.layers.core import Dropout
 from keras import initializations, optimizers
 from keras.callbacks import TensorBoard
-import datetime
+from keras.layers import Dense, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+from keras.regularizers import l2
+import numpy as np
+import platform
+import os
 
+if platform.system()=='Darwin':
+    path_start = '/Users'
+else:
+    path_start = '/home'
 
+PATH = '%s/andreiasodrenichols/galvanize/orpheus' % path_start
 
 class Trainer():
 
@@ -22,20 +31,19 @@ class Trainer():
         self.X_train = nn_input
         self.X_test = nn_input
 
-
+        directory = '%s/models' %PATH
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
 
     def create_model(self, hidden_layers_sizes=[64,32,64], reg=0.001,
-                     dropout=0.5, weight_scale=0.01, lr=0.0001):
+                     dropout=0.5, lr=0.0001):
         '''
         Creates the architecture of the neural netz.
         Embedding vector is the middle hidden layer.
 
         input, hidden layers and output
         '''
-        #def weight_init(shape, name=None):
-        #    return initializations.normal(shape, scale=weight_scale, name=name)
-
 
         def relu_6(x):
             return K.relu(x, max_value=6)
@@ -47,12 +55,15 @@ class Trainer():
         #input layer
         #model.add(Dense(output_dim=hidden_layers_sizes[0], input_dim=INPUT_SIZE, init='weight_init'))
         model.add(Dense(output_dim=hidden_layers_sizes[0], input_dim=INPUT_SIZE, init='he_normal'))
+        model.add(BatchNormalization())
         model.add(Activation(relu_6))
+        model.add(Dropout(dropout))
 
         #hidden layers
         for i, hidden_layer_size in enumerate(hidden_layers_sizes[1:]):
             #model.add(Dense(output_dim=hidden_layer_size, W_regularizer=l2(reg), init=weight_init))
             model.add(Dense(output_dim=hidden_layer_size, W_regularizer=l2(reg), init='he_normal'))
+            model.add(BatchNormalization())
             model.add(Activation(relu_6))
             model.add(Dropout(dropout))
 
@@ -67,27 +78,37 @@ class Trainer():
 
     def train_nn(self, num_epochs=5, batch_size=32):
         log_name = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-        tb = TensorBoard(log_dir='/Users/andreiasodrenichols/galvanize/orpheus/logs/%s' % log_name,
+        tb = TensorBoard(log_dir='%s/logs/%s' % (PATH, log_name),
             histogram_freq=0, write_graph=True, write_images=False)
 
         self.model.fit(self.X_train, self.X_train, nb_epoch=num_epochs, batch_size=batch_size,
                        callbacks=[tb])
+
+        self.model.save('%s/models/model_%s.h5' %(PATH, log_name))
         #loss_and_metrics = model.evaluate(self.X_test, self.X_test, batch_size=batch_size)
 
 
     def get_embeddings(self):
         embedding_vector_fn = K.function([self.model.layers[0].input, K.learning_phase()],
-                                          [self.model.layers[len(self.model.layers)/2 + 1].output])
+                                          [self.model.layers[len(self.model.layers)/2 - 1].output])
 
         embedding_vectors = embedding_vector_fn([self.X_test, 0])[0]
 
         return embedding_vectors
 
 
-
     def get_predictions(self, batch_size=32):
         pred_outputs = model.predict(self.X_test, batch_size=batch_size, verbose=0)
         return pred_outputs
+
+
+    def get_outputs(self, layer_num=0):
+        layer_vector_fn = K.function([self.model.layers[0].input, K.learning_phase()],
+                                          [self.model.layers[layer_num].output])
+
+        output_vectors = layer_vector_fn([self.X_test, 0])[0]
+
+        return output_vectors
 
 
 
